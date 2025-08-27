111c0872e9c3c9eee595bbe9663a22d597698aa3
Riley Newhart <rileynewhart@MWI-rileynewhart-DN92HXQ633.local>
2025-08-27 00:05:46 -0400

    nums to valid chars

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const validHexChars = ['a', 'b', 'c', 'd', 'e', 'f']
+const validHexChars = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 const isValidHex = (value) => {
     if (!value) return false;
 
-    const splitHex = value.toLowerCase().split('');
+    if (value.length != 6) return false;
 
-    if (splitHex.length != 6) return false;
-
-    for (let i = 0; i < splitHex.length; i++) {
-        if (splitHex[0] != '#' || (i > 0 && !validHexChars.includes(splitHex[i]))) return false;
+    for (let i = 0; i < value.length; i++) {
+        if (value[0] != '#' || (i > 0 && !validHexChars.includes(value[i]))) return false;
     }
 
     return true;
@@ -19,3 +17,4 @@ console.log(isValidHex('#abcdef')) // false - to long
 console.log(isValidHex('#abc')) // false - to short
 console.log(isValidHex('#abcdo')) // false - invalid char
 console.log(isValidHex('#aaabb')) // true - valid hex
+console.log(isValidHex('#12345')) // true - valid hex
